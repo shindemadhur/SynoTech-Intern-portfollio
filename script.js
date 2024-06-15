060e2007654496ce0ae7fe78a85a008f0c2c9452
shindemadhur <143925164+shindemadhur@users.noreply.github.com>
2024-06-15 15:08:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var typed = new Typed('#element', {
+        strings: ['Web Developer', 'UI/UX Designer', 'Web Designer', 'App Designer'],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    });
+});
